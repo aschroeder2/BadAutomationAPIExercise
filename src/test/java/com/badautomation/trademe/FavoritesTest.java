3f8cf9b1757eaba3a57a01e832f38a314ce19e7d
Ashley Schroeder <ashley.schroeder@bullhorn.com>
2024-07-10 20:32:36 -0500

    Initial scaffolding

---
+++ src/test/java/com/badautomation/trademe/FavoritesTest.java
@@ -0,0 +1,11 @@
+package com.badautomation.trademe;
+
+import org.testng.annotations.Test;
+
+public class FavoritesTest {
+
+    @Test
+    public void getFavorites() {
+        System.out.println("is scaffolding working?");
+    }
+}
